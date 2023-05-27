651ba352cbbba475aa02acb879a44d5b20c4ddb4
Tushar Kant Saha <64279043+Tusharkant21@users.noreply.github.com>
2023-05-27 14:58:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,98 @@
-/** @format */
-
-let arr = [
-  { id: 1, name: "john", age: "18", marks: 80 },
-  { id: 2, name: "jack", age: "20", marks: 85 },
-  { id: 3, name: "karen", age: "19", marks: 35 },
-];
-
-function PrintStudentswithMap() {
-  //Write your code here , just console.log
-
-  arr
-    .filter((student) => student.marks > 50)
-    .map((student) => console.log(student));
+// Fetch data from the API using .then
+function fetchDataWithThen() {
+  fetch(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      renderTable(data);
+      handleSearch(data);
+      handleSort(data);
+    })
+    .catch((error) => {
+      console.log("Error fetching data:", error);
+    });
 }
 
-function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
-  arr.forEach((student) => {
-    if (student.marks > 50) {
-      console.log(student);
-    }
+// Fetch data from the API using async/await
+async function fetchDataWithAsyncAwait() {
+  try {
+    const response = await fetch(
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+    );
+    const data = await response.json();
+    renderTable(data);
+    handleSearch(data);
+    handleSort(data);
+  } catch (error) {
+    console.log("Error fetching data:", error);
+  }
+}
+
+// Render the data in the table
+function renderTable(data) {
+  const tableBody = document.getElementById("dataBody");
+  tableBody.innerHTML = "";
+
+  data.forEach((item) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+       <td><img src="${item.image}" alt="${item.name}" width="30"></td>
+      <td>${item.name}</td>
+      <td>${item.id}</td>
+   
+      <td>${item.symbol}</td>
+      <td>${item.current_price}</td>
+      <td>${item.total_volume}</td>
+      <td>${item.market_cap}</td>
+      <td>${item.market_cap_change_percentage_24h}</td>
+    `;
+    tableBody.appendChild(row);
   });
 }
 
-function addData() {
-  //Write your code here, just console.log
-  const newStudent = { id: 4, name: "susan", age: "20", marks: 45 };
-  arr.push(newStudent);
-  console.log(arr);
+// Handle search functionality
+function handleSearch(data) {
+  const searchInput = document.getElementById("searchInput");
+  const searchButton = document.getElementById("searchButton");
+
+  searchButton.addEventListener("click", () => {
+    const searchTerm = searchInput.value.toLowerCase();
+    const filteredData = data.filter((item) => {
+      return (
+        item.name.toLowerCase().includes(searchTerm) ||
+        item.symbol.toLowerCase().includes(searchTerm)
+      );
+    });
+    renderTable(filteredData);
+  });
 }
 
-function removeFailedStudent() {
-  //Write your code here, just console.log
-  arr = arr.filter((student) => student.marks >= 50);
-  console.log(arr);
+// Handle sort functionality
+function handleSort(data) {
+  const sortMarketCapButton = document.getElementById("sortMarketCapButton");
+  const sortPercentageButton = document.getElementById("sortPercentageButton");
+
+  sortMarketCapButton.addEventListener("click", () => {
+    const sortedData = [...data];
+    sortedData.sort((a, b) => b.market_cap - a.market_cap);
+    renderTable(sortedData);
+  });
+
+  sortPercentageButton.addEventListener("click", () => {
+    const sortedData = [...data];
+    sortedData.sort(
+      (a, b) =>
+        b.market_cap_change_percentage_24h - a.market_cap_change_percentage_24h
+    );
+    renderTable(sortedData);
+  });
 }
 
-function concatenateArray() {
-  //Write your code here, just console.log
-  const newArray = [
-    { id: 4, name: "tushar", age: "22", marks: 65 },
-    { id: 5, name: "arjun", age: "19", marks: 75 },
-    { id: 6, name: "rahul", age: "21", marks: 90 },
-  ];
-  const combinedArray = arr.concat(newArray);
-  console.log(combinedArray);
+// Main function
+function main() {
+  fetchDataWithThen(); // Use either fetchDataWithThen() or fetchDataWithAsyncAwait() based on your preference
 }
+
+// Run the main function
+main();
